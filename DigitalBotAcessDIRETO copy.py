a5f1217b477a5f0a2357387c6e67dac884d5a504
Brenno Lourran Figueiredo de Morais <33437341+BrennoLourran@users.noreply.github.com>
2022-07-04 16:33:45 -0300

    Atualização de comentarios

--- DigitalBotAcessDIRETO copy.py
+++ DigitalBotAcessDIRETO copy.py
@@ -9,10 +9,11 @@ if __name__ == '__main__':
     acesso = int(input("Digite a quantidade de acessos: "))
     opcao = int(input('Digite 1 para paginas de Single e 2 para Datacenter \n >>'))
 
-
+    #ESCOLHA DE PAIS
     if pais == 1:
         print('-----Utilizando IPs do Brasil-----')
-
+        
+        #ESCOLHA DE SINGLE
         if opcao == 1:
             links = [
             'https://digitalrecovery.com/pt-br/recuperar-hd-notebook/',
@@ -32,8 +33,57 @@ if __name__ == '__main__':
             timing2 = randomtimetwo - (randomtimetwo * 2)
 
             #Sleep Aleatorio
-            sleeping = random.randint(4, 14)
-            sleeping2 = random.randint(3, 20)
+            sleeping = random.randint(10, 15)
+            sleeping2 = random.randint(10, 20)
+            sleeping3 = random.randint(10, 25)
+
+            i = 0
+            while i < acesso:
+                driver = uc.Chrome()
+
+                driver.get(url)
+                time.sleep(sleeping)
+
+                pyautogui.scroll(timing)
+                time.sleep(sleeping2)
+
+                pyautogui.scroll(timing)
+                time.sleep(sleeping3)
+
+                pyautogui.scroll(timing2)
+                time.sleep(sleeping2)
+
+                #CLIQUE NA LOGO
+                driver.find_element_by_xpath('/html/body/div[1]/header[1]/div/div/div/section/div/div[1]/div/div/div/a/img').click()
+                time.sleep(sleeping3)
+                pyautogui.scroll(timing)
+                i = i + 1
+        
+        #ESCOLHA DE DATACENTER
+        if opcao == 2:
+            links = [
+            'https://digitalrecovery.com/pt-br/recuperar-ransomware/',
+            'https://digitalrecovery.com/pt-br/recuperar-raid/',
+            'https://digitalrecovery.com/pt-br/recuperar-storage-nas-das-san/',
+            'https://digitalrecovery.com/pt-br/recuperar-banco-de-dados/',
+            'https://digitalrecovery.com/pt-br/recuperar-maquina-virtual/',
+            'https://digitalrecovery.com/pt-br/recuperar-fitas-magneticas/',
+            ]
+            
+            #ESCOLHA DE URL ALEATORIA
+            url = random.choice(links)
+            print('Acessando a URL: {}'.format(url))
+            
+            #TEMPO ALEATORIO
+            randomtimeone = random.randint(900, 1900)
+            timing = randomtimeone - (randomtimeone * 2)
+
+            randomtimetwo = random.randint(500, 1800)
+            timing2 = randomtimetwo - (randomtimetwo * 2)
+
+            #SLEEP ALEATORIO
+            sleeping = random.randint(10, 15)
+            sleeping2 = random.randint(10, 20)
             sleeping3 = random.randint(10, 25)
 
             i = 0
@@ -58,6 +108,6 @@ if __name__ == '__main__':
                 pyautogui.scroll(timing)
                 i = i + 1
 
-
+    
     elif pais == 2:
         print('Utilizando IPs de Singapura')
