c107fc76ef17290bb72294a19b87103260512648
satootas <abraaoaasato@gmail.com>
2022-07-04 17:04:06 -0300

    atualização sites EUA

--- DigitalBotAcessDIRETO copy.py
+++ DigitalBotAcessDIRETO copy.py
@@ -118,59 +118,18 @@ if __name__ == '__main__':
         
         #ESCOLHA DE SINGLE
         if opcao == 1:
-            links = [
-            'https://digitalrecovery.com/pt-br/recuperar-hd-notebook/',
-            'https://digitalrecovery.com/pt-br/recuperar-hd-externo/',
-            'https://digitalrecovery.com/pt-br/recuperacao-de-pen-drive/',
-            'https://digitalrecovery.com/pt-br/recuperar-cartao-de-memoria/',
-            ]
-
-            url = random.choice(links)
-            print('Acessando a URL: {}'.format(url))
             
-            #Tempo Aleatorio
-            randomtimeone = random.randint(900, 1900)
-            timing = randomtimeone - (randomtimeone * 2)
-
-            randomtimetwo = random.randint(500, 1800)
-            timing2 = randomtimetwo - (randomtimetwo * 2)
-
-            #Sleep Aleatorio
-            sleeping = random.randint(10, 15)
-            sleeping2 = random.randint(10, 20)
-            sleeping3 = random.randint(10, 25)
-
-            i = 0
-            while i < acesso:
-                driver = uc.Chrome()
-
-                driver.get(url)
-                time.sleep(sleeping)
-
-                pyautogui.scroll(timing)
-                time.sleep(sleeping2)
-
-                pyautogui.scroll(timing)
-                time.sleep(sleeping3)
-
-                pyautogui.scroll(timing2)
-                time.sleep(sleeping2)
-
-                #CLIQUE NA LOGO
-                driver.find_element_by_xpath('/html/body/div[1]/header[1]/div/div/div/section/div/div[1]/div/div/div/a/img').click()
-                time.sleep(sleeping3)
-                pyautogui.scroll(timing)
-                i = i + 1
+            print('Não temos SINGLE nos Estados Unidos.')
         
         #ESCOLHA DE DATACENTER
         if opcao == 2:
             links = [
-            'https://digitalrecovery.com/pt-br/recuperar-ransomware/',
-            'https://digitalrecovery.com/pt-br/recuperar-raid/',
-            'https://digitalrecovery.com/pt-br/recuperar-storage-nas-das-san/',
-            'https://digitalrecovery.com/pt-br/recuperar-banco-de-dados/',
-            'https://digitalrecovery.com/pt-br/recuperar-maquina-virtual/',
-            'https://digitalrecovery.com/pt-br/recuperar-fitas-magneticas/',
+            'https://digitalrecovery.com/en/ransomware-recovery/',
+            'https://digitalrecovery.com/en/recover-raid/',
+            'https://digitalrecovery.com/en/recover-database/',
+            'https://digitalrecovery.com/en/recover-storage-nas-das-san/',
+            'https://digitalrecovery.com/en/recover-virtual-machine/',
+            'https://digitalrecovery.com/en/recover-magnetic-tapes/',
             ]
             
             #ESCOLHA DE URL ALEATORIA
